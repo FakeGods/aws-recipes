f1f0b261625774d81e62e0103a16ce03db4f433f
FakeGods <w.leszkowicz99@gmail.com>
2025-12-04 23:56:54 +0100

    ai one more

--- ai-recipe-generator/amplify/backend.ts
+++ ai-recipe-generator/amplify/backend.ts
@@ -5,14 +5,7 @@ import { data } from './data/resource';
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-const backend = defineBackend({
+defineBackend({
   auth,
   data,
-});
-
-// Add hosting configuration
-backend.addOutput({
-  custom: {
-    distributionFolder: 'dist'
-  }
 });
